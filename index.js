90e1dd8fdc6a171b80c48392185ec0949494705b
yutangb3 <yutangb3@gmail.com>
2026-08-04 18:05:12 +0900

    tiktok修正中

--- index.js
+++ index.js
@@ -61,4 +61,79 @@ const school_buttons=document.querySelectorAll('.schoolname_text');
             header_menu.classList.toggle('hide');
         });
     });
+    let pastscrollY=0;
+    window.addEventListener('scroll',()=>{
+        const header=document.querySelector('.frame_header');
+        if(pastscrollY>=window.scrollY){
+            header.classList.add('scroll_hide');
+        }else{
+            header.classList.add('scroll_hide');
+        };
+        pastscrollY=window.scrollY;
+    });
+
+    const scroll_space_2=document.querySelector('.scroll_space_2');
+    const track=document.querySelector('.track');
+    let currentIndex=0;
+    let pastIndex=0;
+    window.addEventListener('scroll',()=>{
+        const lesson_content=document.querySelectorAll('.lesson_content');
+        const lesson_name_time=document.querySelectorAll('.lesson_name_time');
+        const tiktok_scrollY=scroll_space_2.getBoundingClientRect();
+        const lesson_text_transition=document.querySelectorAll('.lesson_text_transition');
+        if(tiktok_scrollY.top<0 && tiktok_scrollY.top>-(584*2)){
+            track.style.transform=`translateY(${tiktok_scrollY.top}px)`;
+        }
+        lesson_content.forEach(content=>{
+            content.classList.remove('active');
+        });
+        lesson_name_time.forEach(name_time=>{
+            name_time.classList.remove('active');
+        });
+        const scroll_position=window.scrollY+tiktok_scrollY.top;
+
+        if(tiktok_scrollY.top>0){
+            lesson_content[0].classList.add('active');
+            lesson_name_time[0].classList.add('active');
+        }else if(tiktok_scrollY.top>-584 && tiktok_scrollY.top<0){
+            pastIndex=currentIndex;
+            currentIndex=0;
+            lesson_content[0].classList.add('active');
+            lesson_name_time[0].classList.add('active');
+            if(pastIndex!=currentIndex){
+                window.scrollTo({
+                    top:scroll_position,
+                    behavior:"smooth"
+                });
+            }
+        }else if(tiktok_scrollY.top<=-584 && tiktok_scrollY.top>-584*2){
+            pastIndex=currentIndex;
+            currentIndex=1;
+            lesson_content[1].classList.add('active');
+            lesson_name_time[1].classList.add('active');
+            if(pastIndex!=currentIndex){
+                window.scrollTo({
+                    top:scroll_position+584,
+                    behavior:"smooth"
+                });
+            }
+        }else if(tiktok_scrollY.top<=-584*2 && tiktok_scrollY.top>-584*3){
+            pastIndex=currentIndex;
+            currentIndex=2;
+            lesson_content[2].classList.add('active');
+            lesson_name_time[2].classList.add('active');
+            if(pastIndex!=currentIndex){
+                window.scrollTo({
+                    top:scroll_position+584*2,
+                    behavior:"smooth"
+                });
+            }
+        }else if(tiktok_scrollY.top>-584*3){
+            lesson_content[2].classList.add('active');
+            lesson_name_time[2].classList.add('active');
+        };
+
+        
+    });
+        
 });
